5d1ead819d22d76713afd55c9cc7173bb05dc493
Rafly Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-04-04 15:20:31 +0700

    Automatic commit: 2025-04-04 15:20

--- python/src/scrapping-komiku.id.py
+++ python/src/scrapping-komiku.id.py
@@ -1,5 +1,4 @@
 import os
-import os
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -19,7 +18,7 @@ def download_image(img_url, img_path, retries=5):
     print(f"Failed to download {img_url} after {retries} attempts.")
 
 def scrape_images():
-    url = 'https://komiku.id/the-s-class-hunter-doesnt-want-to-be-a-villainous-princess-chapter-26/'
+    url = 'https://komiku.id/the-s-class-hunter-doesnt-want-to-be-a-villainous-princess-chapter-24/'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
     
@@ -28,14 +27,23 @@ def scrape_images():
         images = soup.find_all('img', itemprop='image')
         
         image_urls = [img['src'] for img in images if 'src' in img.attrs]
-        
-        # Extract chapter number from URL
-        chapter = url.strip('/').split('-')[-1]
-        folder_name = f"temp/{chapter}"
-        os.makedirs(folder_name, exist_ok=True)
+
+        # Ambil judul halaman
+        title_tag = soup.find('title')
+        if title_tag:
+            title = title_tag.text.strip()
+            # Bersihkan judul dari karakter yang tidak valid untuk nama folder
+            valid_chars = "-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            folder_name = ''.join(c for c in title if c in valid_chars)
+        else:
+            # Jika judul tidak ditemukan, gunakan bagian akhir URL
+            folder_name = url.strip('/').split('/')[-1]
+
+        folder_path = f"temp/{folder_name}"
+        os.makedirs(folder_path, exist_ok=True)
         
         for i, img_url in enumerate(image_urls, start=1):
-            img_path = os.path.join(folder_name, f"image_{i}.jpg")
+            img_path = os.path.join(folder_path, f"image_{i}.jpg")
             download_image(img_url, img_path)
     else:
         print(f"Failed to fetch page, status code: {response.status_code}")
